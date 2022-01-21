0d7ec5f695f1eb5809d526aa401df3d50f68321b
Gleb Zadonskiy <vilomy2@gmail.com>
2022-01-21 14:47:25 +0300

    реализация карточки с помощью SwiftUI

--- business card SwiftUI/ContentView.swift
+++ business card SwiftUI/ContentView.swift
@@ -1,16 +1,47 @@
-//
-//  ContentView.swift
-//  business card SwiftUI
-//
-//  Created by Gleb Zadonskiy on 21.01.2022.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.1, green: 0.74, blue: 0.61).ignoresSafeArea()
+            VStack {
+                Image(systemName: "person.crop.circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(lineWidth: 5))
+                Text("Puss In Boots")
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                Text("IOS developer / Data Scientist")
+                    .font(.headline)
+                    .fontWeight(.light)
+                Divider()
+                Card(image: "phone.fill", message: "+0 (123) 456 78 90")
+                Card(image: "envelope.fill", message: "PussInBoots@gogle.com")
+            }.foregroundColor(.white)
+       
+            
+        }
+    }
+}
+
+struct Card: View {
+    let image: String
+    let message: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .frame(height: 50)
+            .padding(.horizontal)
+            .overlay(
+                HStack {
+                    Image(systemName: image)
+                    Text(message)
+                        .fontWeight(.semibold)
+                }.foregroundColor(Color(red: 0.1, green: 0.74, blue: 0.61))
+            )
     }
 }
 
--- business card SwiftUI/business_card_SwiftUIApp.swift
+++ business card SwiftUI/business_card_SwiftUIApp.swift
@@ -1,9 +1,3 @@
-//
-//  business_card_SwiftUIApp.swift
-//  business card SwiftUI
-//
-//  Created by Gleb Zadonskiy on 21.01.2022.
-//
 
 import SwiftUI
 
