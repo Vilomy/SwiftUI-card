5155ad9d645ca3b08c7bd847a2f5ba595f83d459
Gleb Zadonskiy <vilomy2@gmail.com>
2022-01-21 13:57:51 +0300

    Initial Commit

---
+++ business card SwiftUI/business_card_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  business_card_SwiftUIApp.swift
+//  business card SwiftUI
+//
+//  Created by Gleb Zadonskiy on 21.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct business_card_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
